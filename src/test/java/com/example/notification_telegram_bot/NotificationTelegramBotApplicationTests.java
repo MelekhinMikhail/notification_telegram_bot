ff6f7925a11910a8dff76eb5808d8d1b5da9ac35
Admin <admin@mail.ru>
2023-05-19 13:51:01 +0300

    Initial commit

---
+++ src/test/java/com/example/notification_telegram_bot/NotificationTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notification_telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
